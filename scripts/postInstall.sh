1dadee3ffc8752707123ae41289994c20473469a
Dan Woods <dan.woods@target.com>
2017-05-07 16:47:49 -0500

    enable nginx at startup, disable selinux, and set privs on html content

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+chkconfig nginx on
+
+# YOLO Mode: Disable SELINUX
+echo "SELINUX=disabled" > /etc/sysconfig/selinux
+
+# Stage our HTML files
 rm -rf /usr/share/nginx/html/*
 mv /html/* /usr/share/nginx/html/
+
+# More YOLO Mode
+chmod 777 /usr/share/nginx/html/*
